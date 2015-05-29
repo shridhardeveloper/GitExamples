aad3f724ac3aa1e052b3f419ce0f853c523fe99d
shridhar dev <shridhar.pasodi.developer@gmail.com>
2015-05-29 18:14:32 +0530

    Code with serilog and elastic search.

--- ManyConsoleWithGit/ManyConsoleWithGit/Program.cs
+++ ManyConsoleWithGit/ManyConsoleWithGit/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,46 @@ namespace ManyConsoleWithGit
     {
         static void Main(string[] args)
         {
+            var lists = new List<String> { "Shridhar", "Pasodi" };
+            var collections = new { Name = "Shridhar", Address = "Mumbai", Mobile = 155 };
+            var c1 = new[] { new { Name = "Shridhar", Age = 21 }, new { Name = "Rock", Age = 23 } };
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.ColoredConsole()
+                .WriteTo.Elasticsearch()
+                .CreateLogger();
+            try
+            {
+                var a = 0;
+                var b = 1;
+                var c = b / a;
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error occured: {0}", ex.Message);
+
+                Log.Error("This is an Error on FirstDemo");
+
+                var contextLogger = Log.Logger.ForContext<Program>();
+
+                contextLogger.Information("This is the Context Info for FirstDemo");
+
+                contextLogger.Information("Include a collection {data}", lists);
+                contextLogger.Information("Include complex data {data1}", collections);
+
+                Log.Information("This is the Context Info for FirstDemo");
+
+                Log.Debug("Include a collection {data}", lists);
+                Log.Fatal("Include complex data {data1}", collections);
+
+                Log.Warning("Include complex data {data1}", c1);
+
+                Console.ReadLine();
+            }
+            finally
+            {
+                Log.Information("Finally is called...");
+                Console.ReadLine();
+            }
         }
     }
 }
